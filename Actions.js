c8d79d24f746d39324d78d9f48f66ded6d8ae162
Andrew <andpuc23@gmail.com>
2019-01-24 22:38:34 +0300

    24.01

--- Actions.js
+++ Actions.js
@@ -1,3 +1,5 @@
+var vm = new Vue({});
+
 var file = null;
 function upload(){
     var fileInput = document.getElementById("fileUploader");
@@ -7,3 +9,38 @@ function upload(){
 function sendFile() {
     //throw it on server somehow
 }
+
+function showPercValue(){
+    var percents = document.getElementById("perc_val");
+    var val = document.getElementById("perc_selector");
+    percents.innerText = val.value;
+}
+
+function showAnsValue(){
+    var quantity = document.getElementById("mostPopularA");
+    var val = document.getElementById("AnsVal");
+    val.innerText = quantity.value;
+}
+
+let concrete_questions = [];
+function addCQ() {
+    //добавление элементов в двумерный массив
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
